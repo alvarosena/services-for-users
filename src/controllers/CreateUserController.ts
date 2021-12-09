aa64ce90ac2b08c1628d6b28081d50f2428aaf2c
Alvaro Sena <alvarosenacs.c@gmail.com>
2021-12-08 21:32:34 -0300

    testing deploy v3

--- src/controllers/CreateUserController.ts
+++ src/controllers/CreateUserController.ts
@@ -2,14 +2,14 @@ import { Request, Response } from "express";
 import { container } from "tsyringe";
 import { CreateUserService } from "../services/CreateUserService";
 
-class CreateUserController{
+class CreateUserController {
   async handle(request: Request, response: Response): Promise<Response> {
     const { username, email, password } = request.body;
-    console.log(username);
+    console.log(email);
 
     const createUserUseService = container.resolve(CreateUserService);
 
-    await createUserUseService.execute({username, email, password});
+    await createUserUseService.execute({ username, email, password });
 
     return response.status(201).send();
   }
